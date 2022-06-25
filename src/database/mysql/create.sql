cf5b679db17bcccea5f82d16ddad76022e5b65ed
xupingmao <xupingmao@gmail.com>
2022-06-26 00:01:57 +0800

    Update

---
+++ src/database/mysql/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `my_table_name` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `version` bigint(20) DEFAULT 0,
+  `created_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `user_id` bigint(20) NOT NULL COMMENT '用户ID',
+  PRIMARY KEY (`id`),
+  KEY `idx_user_id` (`user_id`) USING BTREE COMMENT '用户ID索引'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT '表功能描述';
