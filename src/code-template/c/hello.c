90d244f05f447c250b747c30eee2ab9a23b4d4e6
xupingmao <xupingmao@gmail.com>
2022-06-25 23:51:30 +0800

    Update

---
+++ src/code-template/c/hello.c
@@ -0,0 +1,16 @@
+/*
+ * @Author: xupingmao xupingmao@gmail.com
+ * @Date: 2022-06-25 23:50:13
+ * @LastEditors: xupingmao xupingmao@gmail.com
+ * @LastEditTime: 2022-06-25 23:50:24
+ * @FilePath: \duck_rush\src\code-template\c\hello.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("hello,world!\n");
+    return 0;
+}
---
+++ src/code-tools/count-code-cloc.sh
@@ -0,0 +1,11 @@
+###
+ # @Author: xupingmao xupingmao@gmail.com
+ # @Date: 2022-06-25 23:49:02
+ # @LastEditors: xupingmao xupingmao@gmail.com
+ # @LastEditTime: 2022-06-25 23:49:06
+ # @FilePath: \duck_rush\src\code-tools\count-code-cloc.sh
+ # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+### 
+
+# 使用 cloc 统计代码
+cloc .
