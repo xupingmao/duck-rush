cecd1ffa0ffa189c1d14e3c6529cb7a574160ef0
xupingmao <xupingmao@gmail.com>
2023-02-05 13:42:43 +0800

    update

---
+++ src/android/adb_commands.sh
@@ -0,0 +1,11 @@
+###
+ # @Author       : xupingmao
+ # @email        : 578749341@qq.com
+ # @Date         : 2022-07-02 17:21:46
+ # @LastEditors  : xupingmao
+ # @LastEditTime : 2022-07-02 17:23:35
+ # @FilePath     : /xnoted:/projects/duck_rush/src/android/adb_commands.sh
+ # @Description  : 描述
+### 
+
+adb devices # 显示当前的调试设备
---
+++ src/code-template/python/hello.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+"""
+@Author       : xupingmao
+@email        : 578749341@qq.com
+@Date         : 2022-07-02 17:20:56
+@LastEditors  : xupingmao
+@LastEditTime : 2022-07-02 17:21:03
+@FilePath     : /xnoted:/projects/duck_rush/src/code-template/python/hello.py
+@Description  : 描述
+"""
+print("hello,world!")
