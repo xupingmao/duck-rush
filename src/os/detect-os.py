5635e819635cb3a66271345d41f06cefa8fbee1d
xupingmao <xupingmao@gmail.com>
2023-02-05 14:08:02 +0800

    Create detect-os.py

---
+++ src/os/detect-os.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+"""
+@Author       : xupingmao
+@email        : 578749341@qq.com
+@Date         : 2023-02-05 14:03:53
+@LastEditors  : xupingmao
+@LastEditTime : 2023-02-05 14:06:49
+@FilePath     : /xnoted:/projects/duck_rush/src/os/detect-os.py
+@Description  : 描述
+"""
+
+import os
+import sys
+import platform
+
+# os.name 用于判断模块是否可用，只注册了三个值 posix/nt/java
+print("os.name: %s" % os.name)
+# sys.platform 依赖在构建配置时指定的编译器定义
+print("sys.platform: %s" % sys.platform)
+# platform.system() 会返回更详细的系统信息，比如 Linux/Windows/Darwin
+print("platform.system(): %s" % platform.system())
