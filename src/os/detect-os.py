abaa3b7a908db83afdaaf1f5f00262f0b40a173a
xupingmao <xupingmao@gmail.com>
2023-02-05 16:11:44 +0800

    Update detect-os.py

--- src/os/detect-os.py
+++ src/os/detect-os.py
@@ -4,7 +4,7 @@
 @email        : 578749341@qq.com
 @Date         : 2023-02-05 14:03:53
 @LastEditors  : xupingmao
-@LastEditTime : 2023-02-05 14:06:49
+@LastEditTime : 2023-02-05 14:10:02
 @FilePath     : /xnoted:/projects/duck_rush/src/os/detect-os.py
 @Description  : 描述
 """
@@ -13,6 +13,7 @@ import os
 import sys
 import platform
 
+# 参考文档： https://blog.csdn.net/KnownAll/article/details/81560050
 # os.name 用于判断模块是否可用，只注册了三个值 posix/nt/java
 print("os.name: %s" % os.name)
 # sys.platform 依赖在构建配置时指定的编译器定义
