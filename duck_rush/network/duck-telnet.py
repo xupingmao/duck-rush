fe870518f751c68c565c246cba06a0f132088242
xupingmao <xupingmao@gmail.com>
2025-01-12 19:51:26 +0800

    Update duck-telnet.py

--- duck_rush/network/duck-telnet.py
+++ duck_rush/network/duck-telnet.py
@@ -11,25 +11,106 @@ Description: 描述
 
 import socket
 import fire
+import sys
+import traceback
+import typing
+import logging
 
-def check_tcp_port(ip_address: str, port: str):
-    port_int = int(port)
-    socket.setdefaulttimeout(3)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        sock.connect((ip_address, port_int))
+
+class TelnetClient:
+
+    def __init__(self, ip_address: str, port: int):
+        self.ip_address = ip_address
+        self.port = port
+        self.bufsize = 1024
+
+    def run(self):
+        socket.setdefaulttimeout(3)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        if self.ip_address != "" or self.port != 0:
+            self._try_exec(self._connect)
+
+        while True:
+            self._try_exec(self._run_loop)
+    
+    def readuntil(self, ends: bytes):
+        buf = b""
+        while True:
+            tmp_buf = self.sock.recv(self.bufsize)
+            if len(tmp_buf) == 0:
+                return buf
+            buf += tmp_buf
+            if buf.endswith(ends):
+                return buf
+        return buf
+    
+    def _try_exec(self, func: typing.Callable, *args, **kw):
+        try:
+            return func(*args, **kw)
+        except socket.timeout:
+            print("connect timeout")
+        except ConnectionRefusedError:
+            print("connect refused")
+        except TimeoutError:
+            print("connect timeout")
+        except InterruptedError:
+            print("connect interrupted")
+        except Exception as e:
+            print("unknown exception")
+            traceback.print_exc()
+
+    
+    def _connect(self):
+        self.sock.connect((self.ip_address, self.port))
         print(f"connect success")
-    except socket.timeout:
-        print("connect timeout")
-    except ConnectionRefusedError:
-        print("connect refused")
-    except TimeoutError:
-        print("connect timeout")
-    except InterruptedError:
-        print("connect interrupted")
-    except Exception as e:
-        print("unknown exception")
-        raise e
+
+    def _open(self, args: list):
+        if len(args) < 2:
+            print("Bad args: %s", args)
+            return
+        
+        ip_address = args[0]
+        port = int(args[1])
+
+        self.sock.connect((ip_address, port))
+    
+    def _run_loop(self):
+        while True:
+            input_str = input("> ")
+            command, *args = input_str.split()
+            command = command.lower()
+
+            if command == "multi":
+                commands = []
+                while True:
+                    line = input(">> ")
+                    if line.lower() in ("end"):
+                        break
+                    commands.append(line)
+                command = "\r\n".join(commands)
+
+            send_str = command + "\r\n"
+
+            if command in ("quit", "exit"):
+                print("Bye")
+                sys.exit(0)
+                break
+            elif command == "open":
+                self._open(args)
+                break
+            else:         
+                logging.debug("send_str: %r", send_str)
+                send_count = self.sock.send(send_str.encode("utf-8"))
+                buf = self.readuntil(b"\r\n")
+                print(f"recv: {buf}")
+
+def main(ip_address: str, port: str, debug=False):
+    if debug:
+        logging.basicConfig(level=logging.DEBUG)
+    port_int = int(port)
+    client = TelnetClient(ip_address=ip_address, port=port_int)
+    client.run()
 
 if __name__ == "__main__":
-    fire.Fire(check_tcp_port)
+    fire.Fire(main)
