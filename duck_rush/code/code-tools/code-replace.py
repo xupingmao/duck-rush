771cc415f3c8f345a4dab8f176c61463c88a09dd
xupingmao <xupingmao@gmail.com>
2026-04-12 00:06:31 +0800

    优化 code-replace 加类型注解

--- duck_rush/code/code-tools/code-replace.py
+++ duck_rush/code/code-tools/code-replace.py
@@ -7,6 +7,7 @@ import os
 import argparse
 import sys
 import chardet
+from typing import List, Optional, Union, Tuple, TextIO
 
 duck_rush_dir = os.environ.get("DUCK_RUSH_DIR", "")
 if duck_rush_dir not in sys.path:
@@ -25,16 +26,16 @@ CODE_EXT_SET = set([
 ])
 
 # [[func1, args], [func2, args2]]
-COMMANDS = []
+COMMANDS: List[List[Union[callable, List]]] = []
 # 5M
 FILE_SIZE_LIMIT = 1024 * 1024 * 5
 
 class Message:
-    CONFIRM_REPLACE = "Replace Code?(Y/N):"
-    FIND_RESULT = "\nFind Code in %d files"
+    CONFIRM_REPLACE: str = "Replace Code?(Y/N):"
+    FIND_RESULT: str = "\nFind Code in %d files"
 
 MESSAGE = Message()
-def init_language():
+def init_language() -> None:
     global MESSAGE
     MESSAGE.CONFIRM_REPLACE = "是否确认替换?(Y/N):"
     MESSAGE.FIND_RESULT = "\n在%d个文件中找到目标代码"
@@ -42,22 +43,23 @@ def init_language():
 
 class CodeFinder:
 
-    def __init__(self, fpath, source, target):
-        self.fpath  = fpath
-        self.lines  = []
-        self.source = source
-        self.target = target
-        self.encoding = None
+    def __init__(self, fpath: str, source: str, target: str):
+        self.fpath: str = fpath
+        self.lines: List[str] = []
+        self.source: str = source
+        self.target: str = target
+        self.encoding: Optional[str] = None
+        self.text: Optional[str] = None
 
-    def get_result(self):
+    def get_result(self) -> Optional['CodeFinder']:
         if len(self.lines) > 0:
             return self
         return None
 
-    def append(self, line):
+    def append(self, line: str) -> None:
         self.lines.append(line)
 
-    def ask(self):
+    def ask(self) -> None:
         print("\nFile: %s" % self.fpath)
         for line in self.lines:
             print("")
@@ -70,7 +72,7 @@ class CodeFinder:
 
             set_console_font_color("default")
 
-    def readfile(self, fpath, raise_error=True):
+    def readfile(self, fpath: str, raise_error: bool = True) -> str:
         last_err = None
         ENCODING_TUPLE = ("utf-8", "gbk", "mbcs", "latin_1")
 
@@ -83,41 +85,44 @@ class CodeFinder:
             except Exception as e:
                 last_err = e
         if raise_error:
-            raise Exception("can not read file %s" % path, last_err)
+            raise Exception("can not read file %s" % fpath, last_err)
+        return ""
 
-    def do_find(self):
-        text      = self.readfile(self.fpath)
+    def do_find(self) -> None:
+        text = self.readfile(self.fpath)
         self.text = text
 
         for line in text.split("\n"):
             if self.source in line:
                 self.lines.append(line)
 
-    def do_replace(self):
+    def do_replace(self) -> None:
         # 1. backup
-        new_text = self.text.replace(self.source, self.target)
-        with open(self.fpath, encoding = self.encoding, mode = "w+") as fp:
-            fp.write(new_text)
+        if self.text is not None:
+            new_text = self.text.replace(self.source, self.target)
+            with open(self.fpath, encoding = self.encoding, mode = "w+") as fp:
+                fp.write(new_text)
 
-def is_code_file(fpath):
+def is_code_file(fpath: str) -> bool:
     _, ext = os.path.splitext(fpath)
     return ext in CODE_EXT_SET
 
 
-def get_file_size(fpath, format=False):
+def get_file_size(fpath: str, format: bool = False) -> int:
     st = os.stat(fpath)
     if st and st.st_size >= 0:
         return st.st_size
     return -1
 
-def check_file_size(fpath):
+def check_file_size(fpath: str) -> Optional[str]:
     size = get_file_size(fpath)
     if size < 0:
         return "os.stat failed"
     if size > FILE_SIZE_LIMIT:
         return "file too large"
+    return None
 
-def readfile(fpath, raise_error=True):
+def readfile(fpath: str, raise_error: bool = True) -> str:
     last_err = None
     ENCODING_TUPLE = ("utf-8", "gbk", "mbcs", "latin_1")
 
@@ -129,27 +134,30 @@ def readfile(fpath, raise_error=True):
             last_err = e
     if raise_error:
         raise Exception("can not read file %s" % fpath, last_err)
+    return ""
 
 
-def find_in_file(fpath, source, target):
+def find_in_file(fpath: str, source: str, target: str) -> Optional[CodeFinder]:
     finder = CodeFinder(fpath, source, target)
 
     error = check_file_size(fpath)
-    if error != None:
+    if error is not None:
         print("WARN: READ_FILE_FAILED fpath: %s, error: %s" % (fpath, error))
-        return
+        return None
 
     finder.do_find()
     return finder.get_result()
 
 
-def replace_dir(dirname = "./", source = None, target = None):
-    results = []
+def replace_dir(dirname: str = "./", source: Optional[str] = None, target: Optional[str] = None) -> None:
+    results: List[CodeFinder] = []
 
     for root, dirs, files in os.walk(dirname):
         for fname in files:
             fpath = os.path.join(root, fname)
             if not is_code_file(fpath):
+                continue
+            if source is None or target is None:
                 continue
             find_result = find_in_file(fpath, source, target)
             if find_result is None:
@@ -172,7 +180,7 @@ def replace_dir(dirname = "./", source = None, target = None):
             result.do_replace()
 
 
-def main():
+def main() -> None:
     init_language()
     parser = argparse.ArgumentParser(description = "代码替换工具")
     parser.add_argument("source", help = "旧的字符串")
