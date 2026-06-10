5dcfe1f698d90ae35a34d38b8499b851d9de60a5
xupingmao <xupingmao@gmail.com>
2026-06-11 02:26:11 +0800

    Update duck-rename.py

--- duck_rush/fs/duck-rename.py
+++ duck_rush/fs/duck-rename.py
@@ -1,26 +1,38 @@
-
 import xutils
 import os
-import fire
+import argparse
 
 
-class DuckRename:
-    def encode(self, dirname="./"):
-        """对文件名进行base64编码"""
-        for fname in os.listdir(dirname):
-            new_name = xutils.encode_name(fname)
-            old_path = os.path.join(dirname, fname)
-            new_path = os.path.join(dirname, new_name)
-            os.rename(old_path, new_path)
+def encode(dirname="./"):
+    for fname in os.listdir(dirname):
+        new_name = xutils.encode_name(fname)
+        old_path = os.path.join(dirname, fname)
+        new_path = os.path.join(dirname, new_name)
+        os.rename(old_path, new_path)
 
-    def decode(self, dirname="./"):
-        """对文件名进行base64解码"""
-        for fname in os.listdir(dirname):
-            new_name = xutils.decode_name(fname)
-            old_path = os.path.join(dirname, fname)
-            new_path = os.path.join(dirname, new_name)
-            os.rename(old_path, new_path)
+
+def decode(dirname="./"):
+    for fname in os.listdir(dirname):
+        new_name = xutils.decode_name(fname)
+        old_path = os.path.join(dirname, fname)
+        new_path = os.path.join(dirname, new_name)
+        os.rename(old_path, new_path)
 
 
 if __name__ == "__main__":
-    fire.Fire(DuckRename)
+    parser = argparse.ArgumentParser(description="文件名编解码工具")
+    subparsers = parser.add_subparsers(dest="command", help="子命令")
+
+    encode_parser = subparsers.add_parser("encode", help="对文件名进行base64编码")
+    encode_parser.add_argument("dirname", nargs="?", default="./", help="目录路径")
+
+    decode_parser = subparsers.add_parser("decode", help="对文件名进行base64解码")
+    decode_parser.add_argument("dirname", nargs="?", default="./", help="目录路径")
+
+    args = parser.parse_args()
+    if args.command == "encode":
+        encode(args.dirname)
+    elif args.command == "decode":
+        decode(args.dirname)
+    else:
+        parser.print_help()
