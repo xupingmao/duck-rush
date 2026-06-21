5ce443b2980971a417ce45a84649ac1f884d881d
xupingmao <xupingmao@gmail.com>
2026-06-21 22:13:15 +0800

    Update index.config.js

--- duck_rush/web-tools/js/index.config.js
+++ duck_rush/web-tools/js/index.config.js
@@ -41,10 +41,16 @@ const menuConfig = [
         ]
     },
     {
-        id: 'text-split',
-        name: '文本转换工具',
-        url: 'pages/text/text-convert.html',
-        description: "用于将长文本转换为不同格式的工具，方便阅读和处理。"
+        id: "text-tools",
+        name: "文本工具",
+        tabs: [
+            {
+                id: 'text-split',
+                name: '文本转换工具',
+                url: 'pages/text/text-convert.html',
+                description: "用于将长文本转换为不同格式的工具，方便阅读和处理。"
+            }
+        ]
     },
     {
         id: 'json-formatter',
@@ -66,10 +72,16 @@ const menuConfig = [
         description: "用于格式化和验证 JSON 数据的工具，方便阅读和处理。"
     },
     {
-        id: 'run-js',
-        name: '运行JS脚本',
-        url: 'pages/runscripts/runjs.html',
-        description: "用于运行自定义 JavaScript 脚本的工具，方便扩展浏览器功能。"
+        id: 'run-script',
+        name: '运行脚本',
+        tabs: [
+            {
+                id: 'run-js',
+                name: '运行JS脚本',
+                url: 'pages/runscripts/runjs.html',
+                description: "用于运行自定义 JavaScript 脚本的工具，方便扩展浏览器功能。"
+            }
+        ]
     },
     {
         id: 'timestamp-tool',
@@ -113,19 +125,24 @@ const menuConfig = [
         name: "Emoji 选择器",
         url: "pages/emoji.html",
         description: "用于选择和复制 Emoji 字符的工具，方便在设计中使用。"
-
     },
     {
-        id: 'install-user-scripts',
-        name: '安装油猴脚本',
-        url: 'pages/install-user-scripts.html',
-        description: "帮助您安装和管理用户脚本，提升浏览器功能。"
-    },
-    {
-        id: 'localstorage-manager',
-        name: '本地存储管理',
-        url: 'pages/localstorage-manager.html',
-        description: "用于管理浏览器LocalStorage数据的工具，支持添加、编辑、删除、清空和导入/导出操作。"
+        id: "settings",
+        name: "设置",
+        "tabs": [
+            {
+                id: 'install-user-scripts',
+                name: '安装油猴脚本',
+                url: 'pages/install-user-scripts.html',
+                description: "帮助您安装和管理用户脚本，提升浏览器功能。"
+            },
+            {
+                id: 'localstorage-manager',
+                name: '本地存储管理',
+                url: 'pages/localstorage-manager.html',
+                description: "用于管理浏览器LocalStorage数据的工具，支持添加、编辑、删除、清空和导入/导出操作。"
+            }
+        ]
     },
     {
         id: 'examples',
